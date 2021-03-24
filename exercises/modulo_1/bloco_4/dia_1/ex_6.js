1013444f9ef0b22f8042de124563e922a32ba6da
Diego Ribeiro <dihrm2001@gmail.com>
2021-03-24 20:33:30 -0300

    Exercicio corrigido

--- exercises/modulo_1/bloco_4/dia_1/ex_6.js
+++ exercises/modulo_1/bloco_4/dia_1/ex_6.js
@@ -1,28 +1,28 @@
-let piece = '';
+let piece = 'queen';
 let pieceNoSensitiveCase = piece.toLowerCase();
 
 switch (pieceNoSensitiveCase) {
-  case king:
+  case 'king':
     console.log('king -> one square in any direction');
     break;
    
-  case queen:
+  case 'queen':
     console.log('queen -> diagonally, horizontally, or vertically any number of squares');
     break;
    
-  case rook:
+  case 'rook':
     console.log('rook -> horizontally or vertically any number of squares');
     break;
    
-  case bishop:
+  case 'bishop':
     console.log('bishop -> diagonally any number of squares');
     break;
    
-  case knight:
+  case 'knight':
     console.log('knight -> in an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically');
     break;
    
-  case pawn:
+  case 'pawn':
     console.log('pawn -> vertically forward one square, with the option to move two squares if they have not yet moved');
     break;
   default:
