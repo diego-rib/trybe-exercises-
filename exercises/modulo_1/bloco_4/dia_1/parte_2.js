185d6242a0821f32aae4c006811c8e5284478d4f
Diego Ribeiro <dihrm2001@gmail.com>
2021-03-24 15:09:26 -0300

    Exercicios da parte 2 do conteudo feitos

---
+++ exercises/modulo_1/bloco_4/dia_1/parte_2.js
@@ -0,0 +1,14 @@
+const name = "Diego";
+const birthCity = "São João del Rei";
+let birthYear = 2001;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2021
+console.log(birthYear);
+
+birthCity = "São João del Rei - MG";
+console.log(birthCity);
+// a mensagem de erro ocorre por ser uma constante, e constantes não podem ser alteradas
