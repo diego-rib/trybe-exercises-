85e864584edda8a61b2673a825890de42eb39966
Diego Ribeiro <dihrm2001@gmail.com>
2021-03-29 16:13:02 -0300

    Exercicio de funçao - refatorar o codigo dos primeiros exercicios em JavaScript

---
+++ exercises/modulo_1/bloco_4/dia_4/desafio_aula/functions.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 2;
+
+function soma (numeroA, numeroB) {
+  return numeroA + numeroB;
+}
+
+function subtrai (numeroA, numeroB) {
+  return numeroA - numeroB;
+}
+
+function multiplica (numeroA, numeroB) {
+  return numeroA * numeroB;
+}
+
+function divide (numeroA, numeroB) {
+  return numeroA / numeroB;
+}
+
+function modulo (numeroA, numeroB) {
+  return numeroA % numeroB;
+}
