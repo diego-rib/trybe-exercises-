acb4d92c9745fb6e155e28f1c85fe7b43accf690
Diego Ribeiro <dihrm2001@gmail.com>
2021-03-29 20:01:52 -0300

    Exercicio 2.2 - retorna o indice do maior valor do array

--- exercises/modulo_1/bloco_4/dia_4/exercicios/ex_parte_2.js
+++ exercises/modulo_1/bloco_4/dia_4/exercicios/ex_parte_2.js
@@ -1,11 +1,24 @@
+// 1 - Função que verifica palíndromos
 function verificaPalindrome(word) {
   let palindrome = true;
-  let indexInverso = word.length-1;
-  for (let index = 0; index <= indexInverso; index += 1) {
-    if (word[index] !== word[indexInverso]) {
+  let revIndex = word.length-1;
+  for (let index = 0; index <= revIndex; index += 1) {
+    if (word[index] !== word[revIndex]) {
       palindrome = false;
     }
-    indexInverso -= 1;
+    revIndex -= 1;
   }
   return palindrome;
+}
+
+// 2 - Função que retorna o índice do maior número em um array
+function checkHigherIndex (numbers) {
+  let higher = numbers[0];
+  let higherIndex = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > higher) {
+      higherIndex = index;
+    }
+  }
+  return higherIndex;
 }
