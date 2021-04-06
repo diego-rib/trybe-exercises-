4bc81d0563bbac88451afbfa290e435d618cf997
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-06 18:25:16 -0300

    Exercicio 1.5

--- exercises/modulo_1/bloco_5/dia_2/exercicios/script.js
+++ exercises/modulo_1/bloco_5/dia_2/exercicios/script.js
@@ -17,3 +17,6 @@ document.querySelector('.main-content').appendChild(centerContent);
 let paragraph = document.createElement('p');
 paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula mollis arcu ac varius. Pellentesque id lacus eget leo eleifend bibendum. Nam aliquet, risus quis hendrerit ullamcorper, mi metus varius velit, et tincidunt elit massa eu eros. Phasellus lacus felis, volutpat eu elit vel, rhoncus aliquam arcu. Ut congue eget sapien id efficitur. Cras at scelerisque tellus. Vestibulum sodales turpis sed dui condimentum interdum. Quisque sapien turpis, sollicitudin id erat vitae, varius posuere leo. Fusce vel urna justo. Nullam eu bibendum leo, at mattis ligula. Proin ac dolor ac purus fringilla rutrum eget quis diam. Nam mollis aliquam suscipit.';
 document.querySelector('.center-content').appendChild(paragraph);
+
+let leftContent = newElement('div', 'left-content');
+document.querySelector('.main-content').appendChild(leftContent);
