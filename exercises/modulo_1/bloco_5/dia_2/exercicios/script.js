ab4210ff9b1a5c83961a3d13c8de90d40391115c
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-06 18:19:06 -0300

    Exercicio 1.2

--- exercises/modulo_1/bloco_5/dia_2/exercicios/script.js
+++ exercises/modulo_1/bloco_5/dia_2/exercicios/script.js
@@ -1,3 +1,13 @@
+function newElement(tag, classe) {
+  let element = document.createElement(tag);
+  element.className = classe;
+  return element;
+}
+
 let titulo = document.createElement('h1');
 titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(titulo);
+
+let mainContent = newElement('div', 'main-content');
+document.body.appendChild(mainContent);
+
