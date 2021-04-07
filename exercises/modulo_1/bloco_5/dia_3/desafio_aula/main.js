a3d2ffb8f6b4cff382be7eec8ce2196fd99613d2
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-07 16:02:59 -0300

    Exercicio 3 e texto do input some ao clicar fora do elemento

--- exercises/modulo_1/bloco_5/dia_3/desafio_aula/main.js
+++ exercises/modulo_1/bloco_5/dia_3/desafio_aula/main.js
@@ -23,8 +23,8 @@ a cor do mesmo;
 Segue abaixo um exemplo do uso de event.target:
 */
 
-function removeClassTech (element) {
-  let elementClassList = element.classList;
+function removeClassTech (event) {
+  let elementClassList = event.classList;
   for (let index = 0; index < elementClassList.length; index += 1) {
     if (elementClassList[index] === 'tech') {
       elementClassList.remove('tech');
@@ -42,6 +42,19 @@ function addClassTech (element) {
 divUm.addEventListener("click", addClassTech);
 divDois.addEventListener("click", addClassTech);
 divTres.addEventListener("click", addClassTech);
+
+function changeText(event) {
+  let element = document.querySelector('.tech'); 
+  element.innerText = input.value;
+}
+
+input.addEventListener("keyup", changeText);
+
+function cleanInput() {
+  input.value = '';
+}
+
+input.addEventListener("change", cleanInput);
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
