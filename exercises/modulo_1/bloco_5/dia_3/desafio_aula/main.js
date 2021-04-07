18ef1b98b458a9a63ab660733b95d8220b1ad12b
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-07 16:12:54 -0300

    Exercicio 5

--- exercises/modulo_1/bloco_5/dia_3/desafio_aula/main.js
+++ exercises/modulo_1/bloco_5/dia_3/desafio_aula/main.js
@@ -4,25 +4,6 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
-1. Copie esse arquivo e edite apenas ele;
-
-2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-
-3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
-
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
-
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
-
-Segue abaixo um exemplo do uso de event.target:
-*/
-
 function removeClassTech (event) {
   let elementClassList = event.classList;
   for (let index = 0; index < elementClassList.length; index += 1) {
@@ -61,6 +42,17 @@ function redirectPage () {
 }
 
 myWebpage.addEventListener("dblclick", redirectPage);
+
+function changeColor (event) {
+  event.target.style.color = '#2fc18c';
+}
+
+function resetColor (event) {
+  event.target.style.color = 'white';
+}
+
+myWebpage.addEventListener("mouseover", changeColor);
+myWebpage.addEventListener("mouseout", resetColor);
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
