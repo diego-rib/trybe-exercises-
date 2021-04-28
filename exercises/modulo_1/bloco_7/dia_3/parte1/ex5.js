4806bca3283809387e3b0813d673f7437da59f17
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-28 16:02:56 -0300

    Organizaçao dos exercicios em diretorios e arquivos

---
+++ exercises/modulo_1/bloco_7/dia_3/parte1/ex5.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// Testes
