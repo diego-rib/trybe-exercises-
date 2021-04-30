f1e75b493afe68c880bcc7a6d9a73a9df571e907
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-30 20:10:12 -0300

    Bonus - exercicio 3

--- exercises/modulo_1/bloco_8/dia_1/exercicios/bonus_ex1.js
+++ exercises/modulo_1/bloco_8/dia_1/exercicios/bonus_ex1.js
@@ -23,3 +23,12 @@ const battleMembers = { mage, warrior, dragon };
 const dragonDmg = () => Math.floor(Math.random() * (dragon.strength - 15 + 1) + 15);
 
 const warriorDmg = () => Math.floor(Math.random() * ((warrior.strength * 2) - warrior.strength + 1) + warrior.strength);
+
+const mageDmg = () => {
+  if (mage.mana < 15) {
+    return 'Não possui mana suficiente';
+  } else {
+    mage.mana -= 15;
+    return Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence + 1) + mage.intelligence)
+  }
+};
