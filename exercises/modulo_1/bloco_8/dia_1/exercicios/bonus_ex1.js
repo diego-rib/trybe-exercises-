b0eb00f1fbc9ac465e26b8dd879529026fd3711d
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-30 21:06:20 -0300

    Bonus Parte 2

--- exercises/modulo_1/bloco_8/dia_1/exercicios/bonus_ex1.js
+++ exercises/modulo_1/bloco_8/dia_1/exercicios/bonus_ex1.js
@@ -2,20 +2,20 @@ const mage = {
   healthPoints: 130,
   intelligence: 45,
   mana: 125,
-  damage: undefined,
+  damage: 0,
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: undefined,
+  damage: 0,
 };
 
 const dragon = {
   healthPoints: 350,
-  strength: 50,
-  damage: undefined,
+  strength: 60,
+  damage: 0,
 };
 
 const battleMembers = { mage, warrior, dragon };
@@ -26,9 +26,91 @@ const warriorDmg = () => Math.floor(Math.random() * ((warrior.strength * 2) - wa
 
 const mageDmg = () => {
   if (mage.mana < 15) {
-    return 'Não possui mana suficiente';
+    return 0;
   } else {
-    mage.mana -= 15;
     return Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence + 1) + mage.intelligence)
   }
 };
+
+
+const gameActions = {
+  turnWarrior: (warriorDmg) => {
+    let damageDealt = warriorDmg();
+    if (warrior.healthPoints <= 0) {
+      damageDealt = 0;
+    }
+    dragon.healthPoints -= damageDealt;
+    warrior.damage += damageDealt;
+  },
+  turnMage: (mageDmg) => {
+    let damageDealt = mageDmg();
+    if (mage.healthPoints <= 0) {
+      damageDealt = 0;
+    }
+    if (mage.mana < 15) {
+      console.log('Não possui mana suficiente');
+      damageDealt = 0;
+    } else {
+      mage.mana -= 15;
+    }
+    dragon.healthPoints -= damageDealt;
+    mage.damage += damageDealt;
+  },
+  turnDragon: (dragonDmg) => {
+    let damageDealt = dragonDmg();
+    if (dragon.healthPoints <= 0) {
+      dragon.healthPoints = 0;
+      damageDealt = 0;
+    }
+    if (damageDealt > warrior.healthPoints) {
+      warrior.healthPoints = 0;
+    } else {
+      warrior.healthPoints -= damageDealt;
+    }
+    if (damageDealt > mage.healthPoints) {
+      mage.healthPoints = 0;
+    } else {
+      mage.healthPoints -= damageDealt;
+    }
+    dragon.damage += damageDealt * 2;
+  },
+  finalState: () => {
+    const battleMembers = {
+      warrior: {},
+      mage: {},
+      dragon: {},
+    };
+    Object.assign(battleMembers.warrior, warrior);
+    Object.assign(battleMembers.mage, mage);
+    Object.assign(battleMembers.dragon, dragon);
+    return battleMembers;
+  },
+  checkGame: () => {
+    if (dragon.healthPoints <= 0 || (mage.healthPoints <= 0 && warrior.healthPoints <= 0)) {
+      return false;
+    }
+    return true;
+  },
+};
+
+const startGame = () => {
+  let alive = true;
+  let count = 1;
+  while (alive) {
+    gameActions.turnWarrior(warriorDmg);
+    gameActions.turnMage(warriorDmg);
+    gameActions.turnDragon(dragonDmg);
+    console.log(`<--- Turno ${count} --->\n`);
+    console.log(gameActions.finalState());
+    console.log();
+    alive = gameActions.checkGame();
+    count += 1;
+  }
+  if (dragon.healthPoints <= 0) {
+    console.log('O guerreiro e o mago ganharam!!!');
+  } else {
+    console.log('O dragão ganhou!!!!');
+  }
+}
+
+startGame();
