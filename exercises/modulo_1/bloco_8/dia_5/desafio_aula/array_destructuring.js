604c596c483f2ae40ba65196b01b7b98ba1187a2
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-06 15:51:10 -0300

    Array destructuring - ex 2

--- exercises/modulo_1/bloco_8/dia_5/desafio_aula/array_destructuring.js
+++ exercises/modulo_1/bloco_8/dia_5/desafio_aula/array_destructuring.js
@@ -7,3 +7,16 @@ const [ola, saudar] = saudacoes;
 console.log(saudar(ola));
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// <-------------------------------------------------------------------->
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+let aux = [comida, animal, bebida];
+
+[ animal, bebida, comida ] = aux;
+
+console.log(comida, animal, bebida); // arroz gato água
