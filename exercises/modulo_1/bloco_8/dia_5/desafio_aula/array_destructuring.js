0bf25ff7dc346bd8dc6a971a3fe6b8fea89d78be
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-06 16:08:54 -0300

    Exercicio 1

--- exercises/modulo_1/bloco_8/dia_5/desafio_aula/array_destructuring.js
+++ exercises/modulo_1/bloco_8/dia_5/desafio_aula/array_destructuring.js
@@ -8,7 +8,9 @@ console.log(saudar(ola));
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 
+
 // <-------------------------------------------------------------------->
+
 
 let comida = 'gato';
 let animal = 'água';
@@ -20,3 +22,15 @@ let aux = [comida, animal, bebida];
 [ animal, bebida, comida ] = aux;
 
 console.log(comida, animal, bebida); // arroz gato água
+
+
+// <-------------------------------------------------------------------->
+
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+numerosPares = numerosPares.filter(num => num % 2 === 0);
+
+console.log(numerosPares); // [6, 8, 10, 12]
