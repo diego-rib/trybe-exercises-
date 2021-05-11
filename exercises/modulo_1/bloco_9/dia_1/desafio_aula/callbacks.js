c223ee044845e04e112a16a2b60ca79458565179
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-11 13:18:44 -0300

    Exercicio 2

--- exercises/modulo_1/bloco_9/dia_1/desafio_aula/callbacks.js
+++ exercises/modulo_1/bloco_9/dia_1/desafio_aula/callbacks.js
@@ -15,3 +15,21 @@ const getUser = (callback) => {
 
 assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
 assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+
+// Exercicio 2
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUserTimed = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUserTimed(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUserTimed(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
