fe9c7929a6c3df39ef3a878b66f925578912a2df
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-12 14:59:12 -0300

    Exercicio 2

--- exercises/modulo_1/bloco_9/dia_2/exercicios/apiScript.js
+++ exercises/modulo_1/bloco_9/dia_2/exercicios/apiScript.js
@@ -15,5 +15,23 @@ const fetchJoke = () => {
     });
 };
 
+async function lessThan8000() {
+  return new Promise((resolve, reject) => {
+    const squaredNumbers = [];
+    for (let index = 0; index < 10; index += 1) {
+      squaredNumbers.push(Math.round((Math.random() * 49 + 1 ) ** 2));
+    }
+    const sum = squaredNumbers.reduce((acc, cur) => acc + cur, 0);
+    if (sum < 8000) {
+      resolve('Promise resolvida');
+    } else {
+      reject('Promise rejeitada');
+    }
+  })
+  .then(msg => console.log(msg))
+  .catch(msg => console.log(msg));
+}
 
-window.onload = () => fetchJoke();
+lessThan8000();
+
+// window.onload = () => fetchJoke();
