4eb4364a776a20d02e7d017aad9ced0cdb382886
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-18 16:11:26 -0300

    Exercicio 1.3 e 1.4

--- exercises/modulo_1/bloco_10/dia_1/exercicios/ex1.spec.js
+++ exercises/modulo_1/bloco_10/dia_1/exercicios/ex1.spec.js
@@ -10,4 +10,16 @@ describe('Testes das funções encode e decode', () => {
     expect('d32g4').toEqual(encode('diego'));
   });
 
-})
+  it('Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertido nas vogais a, e, i, o, u , respectivamente', () => {
+    expect('diego').toEqual(decode('d32g4'));
+  });
+
+  it('Função encode não troca letras que não são vogais', () => {
+    expect('nlddjnfdgbdsj').toEqual(encode('nlddjnfdgbdsj'));
+  });
+
+  it('Função decode não troca letras, apenas números', () => {
+    expect('O peito do pé de Pedro é preto').toEqual(decode('O peito do pé de Pedro é preto'));
+  });
+
+});
