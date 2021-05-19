89c720efa551755b6ffd4a04d6d1d35cace62a1e
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-19 18:55:23 -0300

    Exercicio 4

---
+++ exercises/modulo_1/bloco_10/dia_2/ex4.spec.js
@@ -0,0 +1,11 @@
+const getRepos = require('./ex4.js');
+
+describe('Testa função getRepos', () => {
+  const url = 'https://api.github.com/orgs/tryber/repos';
+  
+  it('Testa se o repositório "sd-01-week4-5-project-todo-list" está listado', async () => {
+    const repos = await getRepos(url);
+    expect(repos).toContain('sd-01-week4-5-project-todo-list');
+    expect(repos).toContain('sd-01-week4-5-project-meme-generator');
+  });
+});
