214e42aa9f00e18651debc19590d3a0f3a9b8b89
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-19 18:13:57 -0300

    Exercicio 2

--- exercises/modulo_1/bloco_10/dia_2/ex1.spec.js
+++ exercises/modulo_1/bloco_10/dia_2/ex1.spec.js
@@ -2,7 +2,7 @@ const { uppercase } = require('./ex1');
 
 describe('Testa função uppercase', () => {
   it('Transforma as letras de uma palavra em maiúsculas', (done) => {
-    return uppercase('teste', (result) => {
+    uppercase('teste', (result) => {
       expect(result).toMatch(/TESTE/);
       done();
     });
---
+++ exercises/modulo_1/bloco_10/dia_2/ex2.spec.js
@@ -0,0 +1,13 @@
+const { getUserName } = require('./ex2');
+
+describe('Testa função getUserName', () => {
+  it('Quando encontra o usuário pelo ID', () => {
+    expect.assertions(1);
+    return expect(getUserName(4)).resolves.toEqual('Mark');
+  });
+
+  it('Quando NÃO encontra o usuário pelo ID', () => {
+    expect.assertions(1);
+    return expect(getUserName(7)).rejects.toEqual({ error: 'User with 7 not found.' });
+  });
+});
