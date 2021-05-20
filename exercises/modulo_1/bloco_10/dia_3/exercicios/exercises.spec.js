d806a1222d200835e598b86040d6d316e52703c4
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-20 19:31:29 -0300

    Exercicio 1

--- exercises/modulo_1/bloco_10/dia_3/exercicios/exercises.spec.js
+++ exercises/modulo_1/bloco_10/dia_3/exercicios/exercises.spec.js
@@ -25,5 +25,33 @@ describe('Testa a função randomNumber', () => {
     });
   });
 
-  
+  describe('Exercicio 2 função modificada', () => {
+    beforeEach(() => {
+      randomNumber = jest.fn((a, b) => a / b);
+    });
+
+    it('a chamada', () => {
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalled();
+    });
+
+    it('retorno', () => {
+      expect(randomNumber(2, 1)).toBe(1);
+    });
+
+    it('quantas vezes foi chamada', () => {
+      // randomNumber();
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalledTimes(2);
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('parametros que foi chamada [(10, 2), (25, 5)]', () => {
+      randomNumber(10, 2);
+      randomNumber(25, 5);
+      expect(randomNumber).toHaveBeenCalledWith(10, 1);
+      expect(randomNumber).toHaveBeenCalledWith(25, 2);
+    })
+  });
 });
