0e4d00b5fe4241c790985663caf843dd60272614
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-20 20:14:56 -0300

    Exercicio 3 parte 1

--- exercises/modulo_1/bloco_10/dia_3/exercicios/exercises.spec.js
+++ exercises/modulo_1/bloco_10/dia_3/exercicios/exercises.spec.js
@@ -2,6 +2,12 @@ let { randomNumber } = require('./exercises');
 
 describe('Testa a função randomNumber', () => {
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+  });
+
   describe('Exercicio 1 testando a chamada, o retorno e quantas vezes foi chamada', () => {
     beforeEach(() => {
       randomNumber = jest.fn().mockReturnValue(10);
@@ -52,6 +58,36 @@ describe('Testa a função randomNumber', () => {
       randomNumber(25, 5);
       expect(randomNumber).toHaveBeenCalledWith(10, 2);
       expect(randomNumber).toHaveBeenCalledWith(25, 5);
+    });
+  });
+
+  describe('Exercicio 3 função modificada novamente', () => {  
+    beforeEach(() => {
+      randomNumber = jest.fn((a, b, c) => a * b * c);
+    });
+
+    it('a chamada', () => {
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalled();
+    });
+
+    it('retorno', () => {
+      expect(randomNumber(25, 5, 2)).toBe(250);
+    });
+
+    it('quantas vezes foi chamada', () => {
+      randomNumber();
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalledTimes(2);
+      randomNumber();
+      expect(randomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('parametros que foi chamada [(10, 2, 1), (25, 5, 0)]', () => {
+      randomNumber(10, 2, 1);
+      randomNumber(25, 5, 2);
+      expect(randomNumber).toHaveBeenCalledWith(10, 2, 1);
+      expect(randomNumber).toHaveBeenCalledWith(25, 5, 2);
     });
   });
 });
