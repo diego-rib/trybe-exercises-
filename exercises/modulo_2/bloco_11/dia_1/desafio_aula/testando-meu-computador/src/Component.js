76cb13fd487fdcb981638864514e0a6de74372ce
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-24 13:22:42 -0300

    Primeiro React App

---
+++ exercises/modulo_2/bloco_11/dia_1/desafio_aula/testando-meu-computador/src/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MyCode extends React.Component {
+  render() {
+    return <div><h1>Diego Ribeiro</h1><p>Oi, eu sou Diego e tenho 20 anos :P</p></div>
+  }
+}
+
+export default MyCode;
