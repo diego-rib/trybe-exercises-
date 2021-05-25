8785d8c1e7047113f7b3f2b0845b8cca60ef2e35
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-25 15:10:23 -0300

    Exercicio de fixaçao 'lista de componentes e keys'

--- exercises/modulo_2/bloco_11/dia_2/desafio_aula/props/src/index.js
+++ exercises/modulo_2/bloco_11/dia_2/desafio_aula/props/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
 import Image from './Image';
+import Users from './Users';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Users />
     <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
   </React.StrictMode>,
   document.getElementById('root')
