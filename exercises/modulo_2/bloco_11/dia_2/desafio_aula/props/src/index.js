d26f7b73cf9c6709560f4780dc2d62156203a6a4
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-25 14:51:27 -0300

    Exercicio de fixaçao 'composiçao de componentes'

--- exercises/modulo_2/bloco_11/dia_2/desafio_aula/props/src/index.js
+++ exercises/modulo_2/bloco_11/dia_2/desafio_aula/props/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 import App from './App';
+import Image from './Image';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+    <App />
+    <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
   </React.StrictMode>,
   document.getElementById('root')
 );
