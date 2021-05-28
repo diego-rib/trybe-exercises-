9d418f8a8a3f7bfbe69eeae04bd169715078c970
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-28 15:26:25 -0300

    Event handler generico

--- exercises/modulo_2/bloco_12/dia_2/desafio_aula/src/Form.jsx
+++ exercises/modulo_2/bloco_12/dia_2/desafio_aula/src/Form.jsx
@@ -1,38 +1,64 @@
-import React, { Component } from 'react'
+import { Component, createRef } from 'react';
+import './Form.css';
 
 export default class Form extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      saudation: '',
       textArea: '',
+      seeking: false,
     }
 
-    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.showImage = this.showImage.bind(this);
+    this.fileInput = createRef();
   }
 
-  handleTextAreaChange(event) {
-    this.setState({ textArea: event.target.value });
+  showImage() {
+    console.log(this.fileInput.current.files[0].name);
+  }
+
+  handleChange({ target }) {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div>
         <h1>Exercicios de fixação</h1>
-        <form>
+        <form className="form">
           <label>
-            Just do it
-            <select>
-              <option></option>
-              <option>Oi</option>
-              <option>Hello</option>
+            DropDown Select
+            <select 
+              name="saudation" 
+              value={this.state.saudation} 
+              onChange={this.handleChange}
+            >
+              <option value="Option 1">Option 1</option>
+              <option value="Option 2">Option 2</option>
+              <option value="Option 3">Option 3</option>
             </select>
           </label>
-          <br />
-          <br />
           <label>
-            Write it down:
-            <textarea value={this.state.textArea} onChange={this.handleTextAreaChange}></textarea>
+            TextArea:
+            <textarea 
+            name="textArea" 
+            value={this.state.textArea} 
+            onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            <input name="seeking" type="checkbox" onChange={this.handleChange} />
+            CheckBox
+          </label>
+          <label>
+            Upload
+            <input type="file" ref={this.fileInput} onChange={this.showImage} />
           </label>
         </form>
       </div>
