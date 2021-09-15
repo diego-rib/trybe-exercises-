d4e391f90f4b2b12e59d95e0fcac07052cbcde87
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 18:26:03 -0300

    Exercicio 21

--- exercises/backend/bloco_24/dia_3/ex21.js
+++ exercises/backend/bloco_24/dia_3/ex21.js
@@ -1,1 +1,12 @@
 // Utilizando o operador $text , busque por filmes que contenham o termo "vacation"
+
+db.movies.find(
+  {
+    $text: { $search: "vacation" }
+  },
+  {
+    _id: 0,
+    title: 1,
+    description: 1,
+  }
+);
