38103be76c3687b8710cfc54372e8e98e2703bd6
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 17:31:50 -0300

    Exercicio 12

---
+++ exercises/backend/bloco_24/dia_3/ex12.js
@@ -0,0 +1,12 @@
+// Busque os filmes em que o módulo 5 do campo budget seja 0 e que o array category tenha tamanho 2
+
+db.movies.find(
+  {
+    budget: { $mod: [5, 0] },
+    category: { $size: 2 }
+  },
+  {
+    _id: 0,
+    title: 1,
+  }
+);
