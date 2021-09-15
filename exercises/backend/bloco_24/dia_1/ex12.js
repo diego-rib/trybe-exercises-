7f6266a12657104767f3a12a395ec9ba03f83598
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 10:48:14 -0300

    Exercicio 12

--- exercises/backend/bloco_24/dia_1/ex12.js
+++ exercises/backend/bloco_24/dia_1/ex12.js
@@ -1,1 +1,11 @@
 // Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos
+
+db.movies.updateMany(
+  {},
+  { 
+    $unset: {
+      "budget": "",
+      "estimatedBudget": ""
+    }
+  }
+);
