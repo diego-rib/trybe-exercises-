6e8a4297d6b96f84b1dc439b0f43ff1e227f12e6
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 18:22:57 -0300

    Exercicio 19

--- exercises/backend/bloco_24/dia_3/ex19.js
+++ exercises/backend/bloco_24/dia_3/ex19.js
@@ -1,1 +1,12 @@
 // Utilizando o operador $regex , retorne todos os filmes em que a descrição termine com a palavra "humanity."
+
+db.movies.find(
+  {
+    description: { $regex: /humanity.$/ }
+  },
+  {
+    _id: 0,
+    title: 1,
+    description: 1,
+  }
+);
