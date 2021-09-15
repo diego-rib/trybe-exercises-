226071a3cb5a086d941b20b206b934d001c83ddc
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 11:39:57 -0300

    Exercicio 15

--- exercises/backend/bloco_24/dia_1/bonus/ex15.js
+++ exercises/backend/bloco_24/dia_1/bonus/ex15.js
@@ -1,11 +1,15 @@
 // Produza uma query que renomeie os campos de name para hero_name , e de true_name para full_name ; adicione o campo power com valor 100, em todos os documentos
 
 db.xmen.updateMany(
-  { class: "unknown" },
+  {},
   {
     $rename: {
       "name": "hero_name",
       "true_name": "full_name"
+    },
+    $set: { power: 100 },
+    $currentDate: {
+      "lastUpdated": { $type: "timestamp" }
     }
   }
 );
