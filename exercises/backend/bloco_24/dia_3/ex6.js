2d69c72742a7cf8bd93d7ece3eea534ef7107aa0
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 16:41:17 -0300

    Exercicio 6

---
+++ exercises/backend/bloco_24/dia_3/ex6.js
@@ -0,0 +1,12 @@
+// Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings
+
+db.movies.find(
+  {
+    ratings: { $gt: 103 }
+  },
+  {
+    _id: 0,
+    title: 1,
+    ratings: 1
+  }
+);
