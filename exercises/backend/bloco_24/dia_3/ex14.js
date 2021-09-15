59725331a858c7fb5a4a07c29f7a0a73433130f2
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-15 18:04:37 -0300

    Exercicio 15

--- exercises/backend/bloco_24/dia_3/ex14.js
+++ exercises/backend/bloco_24/dia_3/ex14.js
@@ -3,8 +3,8 @@
 db.movies.find(
   {
     $and: [
-      { category: { $in: ["adventure", "family"] } },
       { ratings: { $size: 4 } },
+      { category: { $in: ["adventure", "family"] } },
       { imdbRating: { $gte: 7 } },
     ]
   },
