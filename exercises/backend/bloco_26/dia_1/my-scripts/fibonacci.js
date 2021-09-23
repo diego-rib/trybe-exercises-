38e88f37b4b2f5fa06a0f05b041926182f3f8e27
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-23 16:36:32 -0300

    Exercicio bonus 2 - fibonacci corrigido

--- exercises/backend/bloco_26/dia_1/my-scripts/fibonacci.js
+++ exercises/backend/bloco_26/dia_1/my-scripts/fibonacci.js
@@ -5,11 +5,15 @@ const calcFib = (n) => {
   let fib = 1;
   let nextNumber = 1;
 
+  if (n === 1) return 0; // o primeiro numero é 0
+  if (n <= 3) return 1; // segundo e terceiro numeros são 1
+
   for (let index = 2; index <= n; index += 1) {
     fib = nextNumber; // atualiza com o numero calculado previamente
     nextNumber += previousNumber; // calcula o proximo numero
     previousNumber = fib; // atualiza o numero antecessor
   }
+
   return fib;
 };
 
