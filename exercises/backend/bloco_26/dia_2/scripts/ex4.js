778404bc4bb752c62f9554034cdc3b41a01dc7e9
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-27 16:17:20 -0300

    Exercicio 4.2

--- exercises/backend/bloco_26/dia_2/scripts/ex4.js
+++ exercises/backend/bloco_26/dia_2/scripts/ex4.js
@@ -7,3 +7,22 @@ fs.readFile('simpsons.json', 'utf8')
     // data.forEach(({ id, name }) => console.log(`${id} - ${name}`));
   })
   .catch((err) => console.log(err.message));
+
+
+// 4.2 - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+const infoCharacter = (searchId) => {
+  return new Promise(async (resolve, reject) => {
+    await fs.readFile('simpsons.json', 'utf8')
+      .then((rawdata) => {
+        const data = JSON.parse(rawdata);
+        const character = data.find(({ id }) => Number(id) === searchId);
+        if(character) resolve(character);
+        reject(new Error('id não encontrado'));
+      })
+      .catch((err) => reject(new Error(err.message)));
+  });
+}
+
+infoCharacter(2)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err.message));
