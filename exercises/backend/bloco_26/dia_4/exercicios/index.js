742c9d809fd147aa73d7ee865e4abed3181addfa
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-30 14:50:30 -0300

    Exercicio 3

--- exercises/backend/bloco_26/dia_4/exercicios/index.js
+++ exercises/backend/bloco_26/dia_4/exercicios/index.js
@@ -17,6 +17,20 @@ app.post('/hello', function(req, res) {
   res.json({ message: `Hello, ${name}` });
 });
 
+// 3 - Crie uma rota POST /greetings
+// 3.1 - Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
+app.post('/greetings', function(req, res) {
+  const { name, age } = req.body;
+
+  // 3.2 - Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
+  if (Number(age) > 17) {
+    return res.status(200).json({ message: `Hello, ${name}` });
+  }
+
+  // 3.3 - Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
+  res.status(401).json({ message: "Unauthorized" });
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
