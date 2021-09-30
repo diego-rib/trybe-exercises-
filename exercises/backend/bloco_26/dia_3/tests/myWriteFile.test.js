b109358b93a3b54191b0776288fbc94fd961ed26
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-30 10:55:56 -0300

    Exercicio 5

--- exercises/backend/bloco_26/dia_3/tests/myWriteFile.test.js
+++ exercises/backend/bloco_26/dia_3/tests/myWriteFile.test.js
@@ -1,8 +1,17 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
 
 const myWriteFile = require('../myWriteFile');
 
 describe('Testa a função myWriteFile', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFileSync');
+  });
+
+  after(() => {
+    fs.writeFileSync.restore();
+  });
+
   describe('a resposta é', () => {
     const retorno = myWriteFile('teste.txt', 'A turma 11 é 10!');
     it('uma string', () => {
