d713d98de6227bf4ab328e8a2f5cb78a486ef0e3
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-30 14:49:23 -0300

    Exercicio 2

--- exercises/backend/bloco_26/dia_4/exercicios/index.js
+++ exercises/backend/bloco_26/dia_4/exercicios/index.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Crie uma rota GET /ping
-// Sua rota deve retornar o seguinte JSON: { message: 'pong' }
+// 1 - Crie uma rota GET /ping
+// 1.1 - Sua rota deve retornar o seguinte JSON: { message: 'pong' }
 app.get('/ping', function(_req, res) {
   res.json({ message: 'pong' });
+});
+
+// 2 - Crie uma rota POST /hello
+// 2.1 - Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// 2.2 - Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } 
+app.post('/hello', function(req, res) {
+  const { name } = req.body;
+  res.json({ message: `Hello, ${name}` });
 });
 
 app.listen(3001, () => {
